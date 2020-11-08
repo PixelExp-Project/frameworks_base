07957230f137a1d53e39e70addbd7af4d97f7512
ezio84 <brabus84@gmail.com>
2020-11-08 10:17:01 +0100

    Ambient music: pulse on new music tracks and Now Playing info [1/2]
    * Adapt to Android 12
    
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Change-Id: I064b92afe59d1a611ef42593fdd8ef7f82fa847e
    Signed-off-by: chiru2000 <chiranthangadi123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5399,6 +5399,12 @@ public final class Settings {
          */
         public static final String SHOW_FOURG_ICON = "show_fourg_icon";
 
+        /** Whether to pulse ambient on new music tracks
+         *
+         * @hide
+         */
+        public static final String PULSE_ON_NEW_TRACKS = "pulse_on_new_tracks";
+
         /**
          * Whether the phone vibrates on call connect
          * @hide

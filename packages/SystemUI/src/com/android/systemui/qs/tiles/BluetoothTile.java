a529f064bf91d2f0d928d819b7ac9fed2df91e19
Chiranth A J <chiranth@palladiumos.com>
2022-10-29 14:19:50 +0530

    Revert "QS: Add and use Bluetooth Panel instead of full Settings"
    No use now
    
    This reverts commit 6434aa14657624ca997d7d9a511a7058d9642e1e.

--- packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
@@ -56,7 +56,7 @@ import javax.inject.Inject;
 
 /** Quick settings tile: Bluetooth **/
 public class BluetoothTile extends QSTileImpl<BooleanState> {
-    private static final Intent BLUETOOTH_SETTINGS = new Intent(Settings.Panel.ACTION_BLUETOOTH);
+    private static final Intent BLUETOOTH_SETTINGS = new Intent(Settings.ACTION_BLUETOOTH_SETTINGS);
 
     private final Handler mHandler;
     private final BluetoothController mController;
@@ -97,7 +97,7 @@ public class BluetoothTile extends QSTileImpl<BooleanState> {
 
     @Override
     public Intent getLongClickIntent() {
-        return BLUETOOTH_SETTINGS;
+        return new Intent(Settings.ACTION_BLUETOOTH_SETTINGS);
     }
 
     @Override
